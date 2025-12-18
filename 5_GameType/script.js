c7c6cc1df4ce33c8496bda737609958b730119ad
SaramanderX <wbworaprat2@gmail.com>
2025-12-18 17:04:37 +0700

    EP5_unfinish

--- 5_GameType/script.js
+++ 5_GameType/script.js
@@ -10,18 +10,23 @@ const levelFormEL = document.getElementById('level-form');
 const levelEL = document.getElementById('level');
 const gameoverEL = document.getElementById('gameover-container');
 
+
 //
-const words = ["เศรษฐี","แมว","กำเนิด","ศักดิ์สิทธิ์","กาญจนบุรี"]
+const words_1 = ["แมว","กำเนิด","ปลาหมอ","หมา"]
+const words_2 = ["ศักดิ์สิทธิ์","เศรษฐี","วงศ์ผู้ดี","กาญจนบุรี"]
 
 
+let result_matching = [] //ที่รับค่าผลการ matching
 // คำต้องเปลี่ยนไปเมื่อป้อนคำถูก คำต้องถูกสุ่มขึ้นมา
+
+
 
 // เมื่อเปิดเว็ป
 function init(){
-    let Use_randomWord = RandomWord(words); //คำสุ่มขึ้นมา
-    console.log(Use_randomWord);
-    let result_check= CheckMatchWords(Use_randomWord); //ผลการเช็คการเท่ากันของคำ
-    console.log(result_check);
+    gameoverEL.style.display='none';
+    RandomWord(words_1); //คำสุ่มขึ้นมา
+    CheckMatchWords(); //ผลการเช็คการเท่ากันของคำ แล้วทำการ เพิ่มค่าสามาชิกใน result_matching
+    console.log(result_matching);
 }
 
 // สุ่มคำโดยสุ่มค่า index และใช้ value จาก array 
@@ -34,13 +39,14 @@ function RandomWord(w){
     wordElement.innerText= words_random;
     //เช็คผลเมื่อผู้ใช้กรอกคำแล้วถูกต้อง โดยโยนค่าที่สุ่มมาแล้วเข้าไปเช็คผลใน function CheckMatchWords
     let textContent= wordElement.textContent
+    Timing(); // เมื่อมีการเปลี่ยนคำให้เวลาทำงานใหม่
     return textContent;
     
 }
 
 
 // ฟังก์ชั่น เช็คผลการพิม
-function CheckMatchWords(textContent){
+function CheckMatchWords(){
     // รับค่าการพิม และเช็คการกดปุ่ม
     textElement.addEventListener('keydown',function(event){
         // เช็คว่าเป็นปุ่ม Enter หรือไม่
@@ -48,19 +54,83 @@ function CheckMatchWords(textContent){
             const text_input = textElement.value
             console.log(text_input)
             //ถ้า text input ตรงกับ text ที่อยู่หน้าจอ ให้ส่งค่าออกไปเป็นจริง ไม่ใช่ เป็น false
-            if(text_input === textContent){
-                console.log("matching")
+            //เมื่อการ enter เรียก function เวลา
+            if(text_input === wordElement.textContent){
+                result_matching.push("matching") //เพิ่มค่าเข้าไปในสมาชิก array
+                let result = "matching"
+                console.log(result)
                 textElement.value = ''; //เปลี่ยนให้ช่องป้อนเป็นค่าว่างหลังพิมแล้ว enter
-                return true;
+                // ทำการสุ่มคำใหม่เมื่อถูกต้อง
+                RandomWord(words_1);
+                //ระบบเพ่ิมคะแนน
+                score_manage(result);
+                console.log("array เพิ่มสมาชิก",result_matching);
             }else{
+                result_matching.push("un-matching")
                 console.log("un-matching")
                 textElement.value = '';//เปลี่ยนให้ช่องป้อนเป็นค่าว่างหลังพิมแล้ว enter
-                return false;
             }
-        }else{
-            return false;
         }
     })
 }
+// ระบบเพิ่มคะแนน 
+function score_manage(result){
+    score = parseInt(scoreElement.innerText); //เมื่อรับค่ามาจาก innertext จะได้ string ต้องแปลงเป็น number ด้วย parseInt
+    if (result === "matching"){
+        score+=10;
+        console.log(score);
+        scoreElement.innerText = `${score}` //เพิ่มค่าแล้วนำไปแสดงที่ html 
+        return score;
+    }else{
+        console.log(score);
+    }
+}
+// 1. ประกาศตัวแปรเก็บ "ตัวจับเวลา" ไว้ข้างนอกสุด (Global)
+// เพื่อให้ทุกฟังก์ชันมองเห็นและสั่งหยุดมันได้
+let timeInterval;
 
+//ระบบเวลา ระบบนับถอยหลัง" (Countdown Timer) : setInterval ให้เรียกใช้ funtion ตามเวลาที่กำหนด clearIntervalหยุด function
+function Timing(){
+    //ถ้าโหมดง่าย 15วิ พิมได้ + 3วิ
+    // ถ้าโหมดปานกลาง 10วิ พิมได้+ 2วิ
+    // ถ้าโหมดยาก 6 วิ + 2วิ
+
+    // กฎเหล็ก: สั่งหยุดตัวจับเวลาเก่าก่อนเสมอ! (ถ้ามี)
+    // ถ้าไม่สั่งบรรทัดนี้ "ผีนาฬิกา" จะโผล่มาซ้อนกัน
+    clearInterval(timeInterval);
+
+    let add_time=0;
+    // เพิ่มค่าเวลาที่ตอบถูกให้กับ เวลาเริ่มต้น
+    let time = 15;
+
+    //เพิ่มเวลา เมื่อตอบถูก
+    if (result_matching.at(-1)==="matching"){
+        add_time = 3;
+    }
+    // รวมเวลาเริ่มต้นใหม่ (เช่น 15 + 3 = 18)
+    time +=add_time;
+
+    timeElement.innerText = time;
+
+    timeInterval = setInterval(updateTimer,1000); //ทุก 1000 มิลิวิ ให้เรียก function updatetime
+    function updateTimer(){
+        time-- // 1.ลดเวลา
+        timeElement.innerText = time ; //2.แสดงผลเลข time ใหม่
+        // 3. เช็คว่าหมดเวลาหรือยัง?
+        if (time<=0){
+            clearInterval(timeInterval); //หยุดการนับเวลาหรือการเรียก function
+            GameOver(); //function เรียกหน้า เมื่อจบเกม
+        };
+    };
+};
+
+GameOver()
+function GameOver(){
+    // ปแบบคำสั่ง: element.style.ชื่อProperty = "ค่าที่ต้องการ";
+    gameoverEL.style.display='flex';
+}
+//Mode selection
+
+
+console.log(gameoverEL)
 init(); 
